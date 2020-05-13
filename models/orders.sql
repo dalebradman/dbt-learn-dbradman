e67931a81b2322df4af456ee903675d7c59479cb
Dale Bradman <dale.bradman@babylonhealth.com>
2020-05-13 16:48:50 +0000

    Added tests

--- models/orders.sql
+++ models/orders.sql
@@ -2,13 +2,17 @@ WITH orders as (
     select * from {{ ref('stg_orders') }}
 ),
 payments as (
-    select * from {{ ref('stg_payments') }}
+    select 
+        orderID as order_id,
+        SUM(amount) as amount
+    from {{ ref('stg_payments') }}
+    group by orderID
 )
 
 select
-    orders.order_id,
-    orders.customer_id,
-    orders.order_date,
-    orders.status,
-    payments.amount
-from orders LEFT JOIN payments ON orders.order_id = payments.orderID
+        orders.order_id as order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        payments.amount
+from orders LEFT JOIN payments ON orders.order_id = payments.order_id
